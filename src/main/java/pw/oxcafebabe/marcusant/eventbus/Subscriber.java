14f60597c223d17a82746a5c2784205e2aaca405
Kazukuta <donotmail@example.com>
2014-08-03 16:40:26 -0500

    added subscriber comparator

--- src/main/java/pw/oxcafebabe/marcusant/eventbus/Subscriber.java
+++ src/main/java/pw/oxcafebabe/marcusant/eventbus/Subscriber.java
@@ -1,5 +1,7 @@
 package pw.oxcafebabe.marcusant.eventbus;
 
+import java.util.Comparator;
+
 /**
  * Receives events
  * Date: 8/3/14
@@ -20,5 +22,15 @@ public interface Subscriber<ET extends Event> {
      * @return priority
      */
     public int priorityValue();
+
+    /**
+     * Compares two subscribers by priority
+     */
+    public static class SubscriberComparator implements Comparator<Subscriber> {
+        @Override
+        public int compare(Subscriber o1, Subscriber o2) {
+            return o2.priorityValue() - o1.priorityValue();
+        }
+    }
 
 }

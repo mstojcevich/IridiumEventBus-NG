ebe3f7d9bdf742895b12955b9dd7adec36fb4e72
Kazukuta <donotmail@example.com>
2014-08-03 15:45:49 -0500

    Added event subscriber interface

---
+++ src/main/java/pw/oxcafebabe/marcusant/eventbus/Subscriber.java
@@ -0,0 +1,17 @@
+package pw.oxcafebabe.marcusant.eventbus;
+
+/**
+ * Receives events
+ * Date: 8/3/14
+ * Time: 3:44 PM
+ * @author Kazukuta
+ */
+public interface Subscriber<ET extends Event> {
+
+    /**
+     * Called upon receipt of an event
+     * @param event event
+     */
+    public void eventReceived(ET event);
+
+}
